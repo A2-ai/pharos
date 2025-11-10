49faab29499fcdcb543c3168f8c37259e5e5558f
Matt Smith <msmith21794@gmail.com>
2025-11-10 12:08:55 -0500

    making get_model_parameter_names public

--- hyperion/src/rust/nonmem/src/model/parameters.rs
+++ hyperion/src/rust/nonmem/src/model/parameters.rs
@@ -136,8 +136,6 @@ pub fn get_parameters(
 ///
 /// @param model hyperion_nonmem_model object from read_model()
 /// @return Named character vector with NONMEM names as names and user-friendly names as values
-/// @keywords internal
-/// @noRd
 ///
 /// @examples \dontrun{
 /// model <- read_model("run001.mod")
