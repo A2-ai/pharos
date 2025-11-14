00aed723900784bfc2250c2ea41313340fe79121
Matt Smith <msmith21794@gmail.com>
2025-11-14 16:23:07 -0500

    making ShkReader structs public

--- components/nonmem/src/output_files/shk.rs
+++ components/nonmem/src/output_files/shk.rs
@@ -232,8 +232,8 @@ impl From<RawShkTable> for Vec<ShkTable> {
 
 #[derive(Debug, Clone)]
 pub struct ShkReader {
-    only_last: bool,
-    only_method: Option<EstimationMethod>,
+    pub only_last: bool,
+    pub only_method: Option<EstimationMethod>,
 }
 
 impl Default for ShkReader {
