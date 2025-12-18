bfface9c5e63d59a69734962ec0c6082d28374cd
Matt Smith <msmith21794@gmail.com>
2025-12-18 10:44:45 -0500

    fixed test for omega parameterization default. updated metadata files

--- hyperion/tests/testthat/test-extract_raw_omega_parts.R
+++ hyperion/tests/testthat/test-extract_raw_omega_parts.R
@@ -36,7 +36,7 @@ test_that("parse omega comments works", {
 	om_comment <- parse_raw_omega_comment("OMEGA(2,1)",	NULL, "OM2,1 CL-VC")
 	expect_equal(om_comment@nonmem_name, "OMEGA(2,1)")
 	expect_equal(om_comment@name, "OM2,1")
-	expect_equal(om_comment@parameterization, "Identity")
+	expect_equal(om_comment@parameterization, NULL)
 	expect_equal(om_comment@associated_theta, c("CL", "VC"))
 
 	om_comment <- parse_raw_omega_comment("OMEGA(2,1)",	NULL, "OM2,1 CL-VC ;log")
