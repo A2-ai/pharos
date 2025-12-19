386405aabea3e0d36983efe6106d9844576efd8f
Matt Smith <msmith21794@gmail.com>
2025-12-19 13:08:32 -0500

    showing cv for fixed params if it's not 0

--- hyperion/R/tables.R
+++ hyperion/R/tables.R
@@ -322,8 +322,8 @@ compute_variability <- function(
   use_sd <- !"sd" %in% drop_columns
 
   dplyr::case_when(
-    fixed ~ NA_character_,
-    use_cv & !is.na(cv) ~ sprintf("(CV = %s%%)", format_sigfig(cv, n_sigfig)),
+    use_cv & !is.na(cv) & cv != 0 ~
+      sprintf("(CV = %s%%)", format_sigfig(cv, n_sigfig)),
     use_corr & !is.na(corr) ~
       sprintf("(Corr = %s)", format_sigfig(corr, n_sigfig)),
     use_sd & !is.na(sd) ~ sprintf("(SD = %s)", format_sigfig(sd, n_sigfig)),
